3553074450a1dbcd9ac79364c0a667c99397b0f6
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-04-30 22:06:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5119722771:AAEZdtiK7fc4rf6oD7vUDFf3nTDbaa76ZWU")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5303569553:AAHMS6Wt8VHUcT8uVyWiSKll1CxuRtkjZlI")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "3393749"))
@@ -32,13 +32,13 @@ DB_NAME = os.environ.get("DATABASE_NAME", "UploadLinkToFileBot")
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "10"))
 
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "").split()):
+    for x in (os.environ.get("ADMINS", "1365052525 5963138883").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
