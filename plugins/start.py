6fd0f2cffb6ab69d9eaf98b076186976b13d7ebb
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-04-30 22:54:04 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,8 +1,4 @@
 #(©)CodeXBotz
-
-
-
-
 import os
 import asyncio
 from pyrogram import Client, filters, __version__
@@ -14,9 +10,6 @@ from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
-
-
-
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
