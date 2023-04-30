473f6cfc5a8befde2576d729cd7e45d124403bed
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-04-30 22:32:47 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -8,7 +8,7 @@ from plugins.link_generator import get_short
 from bot import Bot
 from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
 from helper_func import encode
-from plugins.start_command import media
+from plugins.start import start_command.media
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
 async def channel_post(client: Client, message: Message):
