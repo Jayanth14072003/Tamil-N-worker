ebe82570ab55b1598081681d3f84ad2af55bb477
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-04-30 23:04:38 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -60,6 +60,7 @@ async def start_command(client: Client, message: Message):
 
             if bool(CUSTOM_CAPTION) & (bool(msg.document) or bool(msg.video)):
                 media = msg.video or msg.document
+                global mediaa = media.file_name
                 caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = media.file_name)
             else:
                 caption = "" if not msg.caption else msg.caption.html
